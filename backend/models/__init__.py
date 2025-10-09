13d1e3694aa0464ebb069290a26f19156ac76cd9
Andrey Sadkov <ad.sadkov@gmail.com>
2025-10-09 18:32:44 +0300

    refactor: refactor `__init__.py` files

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,1 +1,4 @@
-from .currency import ExchangeRates, HistoricalRate
+from .currency import ExchangeRates
+from .currency import HistoricalRate
+
+__all__ = ["ExchangeRates", "HistoricalRate"]
--- backend/repositories/__init__.py
+++ backend/repositories/__init__.py
@@ -1,1 +1,3 @@
 from .rates_repository import RatesRepository
+
+__all__ = ["RatesRepository"]
